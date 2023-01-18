09c723a81fef395bcd7bb6baecf079c075e18796
O.Khalil <o.khalil@egyptianbanks.net>
2023-01-18 15:54:57 +0200

    demos

---
+++ Section+7/S07L01 -Section 7 - Adding record(s) into database/src/org/studyeasy/jsf/beans/UsersOperations.java
@@ -0,0 +1,40 @@
+package org.studyeasy.jsf.beans;
+
+import javax.faces.bean.ManagedBean;
+
+import org.studyeasy.jsf.model.DBHelper;
+import org.studyeasy.jsf.model.User;
+
+
+
+@ManagedBean(name="usersOpt")
+public class UsersOperations {
+    String username,email;
+    
+	public UsersOperations() {
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String addUser() {
+		// System.out.println(username);
+		// System.out.println(email);
+		User user = new User(username, email);
+		new DBHelper().addUser(user);
+		return "index";
+	}
+    
+}
